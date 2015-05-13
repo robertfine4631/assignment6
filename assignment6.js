11d03460d832fff32d9acd753a41c93e77066675
Robert Fine <robertfine4631@icloud.com>
2015-05-13 15:27:48 -0400

    initial commit

---
+++ assignment6.js
@@ -0,0 +1,105 @@
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+////    ______                _   _                ////
+////    |  ___|              | | (_)               ////
+////    | |_ _   _ _ __   ___| |_ _  ___  _ __     ////
+////    |  _| | | | '_ \ / __| __| |/ _ \| '_ \    ////
+////    | | | |_| | | | | (__| |_| | (_) | | | |   ////
+////    \_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|   ////
+////                                               ////
+////                                               ////
+////    ______               _   _                 ////
+////    | ___ \             | | (_)                ////
+////    | |_/ / __ __ _  ___| |_ _  ___ ___        ////
+////    |  __/ '__/ _` |/ __| __| |/ __/ _ \       ////
+////    | |  | | | (_| | (__| |_| | (_|  __/       ////
+////    \_|  |_|  \__,_|\___|\__|_|\___\___|       ////
+////                                               ////
+////                                               ////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+
+// Q1
+// Write a function called `sum` that takes two
+// parameters and returns the sum of those 2 numbers.
+
+
+
+function sum (x,y) {
+	return Number(x) + Number(y);
+
+};
+
+
+
+
+
+// Q2
+// Write a function named `avg` that takes 3 parameters
+// and returns the average of those 3 numbers.
+
+function avg (number, number, number) {
+	return (number + number + number)/3;
+}
+
+
+// Q3
+// Write a function called `getLength` that takes one
+// parameter (a string) and returns the length
+
+function getLength (x) {
+	return x.length
+}
+
+
+// Q4
+// Write a function called `greaterThan` that takes
+// two parameters and returns `true` if the second
+// parameter is greater than the first. Otherwise
+// the function should return `false`.
+
+function greaterThan (number1, number2) {
+	if { 
+		number1 < number2;
+		console.log("True");
+
+	};
+	else {
+		console.log("False");
+	}
+};
+
+
+
+// Q5
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+
+var name1 = Bobby;
+
+function greet () {
+	console.log("Hello, " + name1)
+}
+
+
+// Q6
+// Write a function called `madlib` that takes
+// 4 or more parameters (words). The function 
+// should insert the words into a pre-defined
+// sentence. Finally the function should return
+// that sentence.
+// Note: When I say words and sentence I mean
+// strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+
+function madlib (noun1, noun2, noun3, noun4) {
+	console.log(noun1, + " " + noun2 + " " + noun3 + " " + noun4 + "are studying to be the javascripters in town!");
+}
+
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
